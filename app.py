582fcae8cce87131f7f8e2385d574bb118933f5a
Serena <serenaqkong@gmail.com>
2021-07-08 17:24:12 -0700

    added session, protecting questions, flasg msgs

--- app.py
+++ app.py
@@ -8,53 +8,60 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-response = []
-question_count = 0
-
 @app.route("/")
 def start():
-    """ TODO """
+    """ Starting page of survey. """
+
+    session["response"] = []
 
     return render_template("survey_start.html",
-                    survery_title = survey.title,
-                    survey_instructions = survey.instructions)
+                            survery_title = survey.title,
+                            survey_instructions = survey.instructions)
 
 @app.route("/begin", methods=["POST"])
 def begin():
-    """ TODO """
+    """ Clicking on continue button will redirect to survey_question. """
 
-    print(request.form)
     return redirect("/questions/0")
 
 @app.route("/questions/<int:question_count>")
 def survey_question(question_count):
-    """ TODO """
+    """ Generate page for the survey questions.
+        If not yet answered a question and try to change url, will redirect to correct question.
+        If finished survey and tries to go back to a question, will redirect to /thanks """
 
-    return render_template("question.html", question = survey.questions[question_count], count=question_count+1)
+    question_num = len(session["response"])
 
-# @app.route("/questions")
-# def question_redirect(question_count):
-#     """ TODO """
-
-#     return redirect("question.html", question = survey.questions[question_count])
+    if question_num == len(survey.questions):
+        return redirect("/thanks")
+    elif question_count == question_num :
+        return render_template("question.html", 
+                                question = survey.questions[question_num])
+    else:
+        flash("You are trying to access an invalid question")
+        return redirect(f"/questions/{question_num}")
+    # return redirect(url_for(".survey_question", question_count = question_num))
 
 @app.route("/answer", methods=["POST"])
 def next_question():
-    """ TODO """
-    # breakpoint()
+    """ Appends question answer to response and 
+        redirects user to next question or thank you page. """
+
     form_response = request.form["answer"]
-    count = request.form["count"]
+
+    response = session["response"]
     response.append(form_response)
+    session["response"] = response
+    
+    question_num = len(session["response"])
 
-    if int(count) < len(survey.questions):
-        return redirect(url_for(".survey_question", question_count=count))
-    else:
-        return redirect("/thank")
+    return redirect(f"/questions/{question_num}")
 
-@app.route("/thank")
+
+@app.route("/thanks")
 def thank_user():
-    """ TODO """
-    breakpoint()
+    """ Generates completion.html and thanks the use for completing survey """
+
     return render_template("completion.html")
 
 
